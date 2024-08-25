33b046d26298b5d7fd818a51580624b4e3d44757
Wenda Liu <63420806+liu-wen-da@users.noreply.github.com>
2024-08-25 13:19:01 -0700

    added the about section

--- Verison_2.0/src/components/Header.jsx
+++ Verison_2.0/src/components/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
     setOpenNavigation(false);
   };
   return (
-    <div className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+    <div className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}> 
         <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
@@ -37,9 +37,7 @@ const Header = () => {
           <a calssName="block w-[12rem] xl:mr-8" href="#hero">
             <img src={brainwave} width={190} height={40} alt="Iron Wok" />
           </a>
-          <nav className={`${
-            openNavigation ? "flex" : "hidden"
-          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}>
+          <nav className={`${openNavigation ? "flex" : "hidden"} fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}>
               <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
                   {navigation.map((item) => (
                     <a
@@ -48,7 +46,7 @@ const Header = () => {
                       onClick={handleClick}
                       className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
                         item.onlyMobile ? "lg:hidden" : ""
-                      } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                      } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold  ${
                         item.url === pathname.hash
                           ? "z-2 lg:text-n-1"
                           : "lg:text-n-1/50"
@@ -61,6 +59,7 @@ const Header = () => {
               </div>
               <HamburgerMenu/>
           </nav>
+          
 
           <Button className="ml-auto lg:hidden" px="px-3" onClick={toggleNavigation}>
             <MenuSvg openNavigation={openNavigation}/>
--- Verison_2.0/src/components/Information.jsx
+++ Verison_2.0/src/components/Information.jsx
@@ -1,15 +1,24 @@
-
+import Button from './Button'
 import Section from './Section'
 
 const Information = () => {
     return (
 
-        <Section className="pt-[12rem] -mt-[5.25]" crosses crossesOffset="lg:translate-y-[5.25rem]" customePadddings id="Information">
+        <Section className="pt-[12rem] -mt-[5.25rem]" crosses crossesOffset="lg:translate-y-[5.25rem]" customePadddings id="Information">
             <div className="container relative">
                 <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
                     <h1 className="h1 mb-6">
                         Iron Wok
                     </h1>
+                    <p className="body-1 mas-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+                        Iron Wok is a family-owned Chinese restaurant that has been serving both Americanized and authentic Chinese cuisine for the past seven years. We strive to stay deeply connected to our roots and the community we serve, embracing simplicity, a sense of humor, and a genuine love for honest food, good company, and shared experiences.
+                    </p>
+                    <Button href="/Menu" white>
+                        food menu
+                    </Button>
+                </div>
+                <div>
+
                 </div>
             </div>
 
